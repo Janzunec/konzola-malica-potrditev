04b72554da8dd77d4222cd8cc2a61a2918e48b60
encryptedDegen <jan.zunec@sers.si>
2022-06-16 23:10:52 +0200

    installed redux toolkit + started API integration

--- src/pages/Prijava/Prijava.js
+++ src/pages/Prijava/Prijava.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import style from './Prijava.module.css';
 
 const Prijava = () => {
