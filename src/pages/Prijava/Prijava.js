5c97f7154b79a138cab614badcade5116ec3740d
encryptedDegen <jan.zunec@sers.si>
2022-06-18 12:00:23 +0200

    Added user checking + fetching user data

--- src/pages/Prijava/Prijava.js
+++ src/pages/Prijava/Prijava.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { API_KEY } from '../../private/API_KEYS';
 import style from './Prijava.module.css';
 
 const Prijava = () => {
@@ -19,8 +20,44 @@ const Prijava = () => {
 
 	const submitFunctionHandler = (e) => {
 		e.preventDefault();
-		if (zaposleniID === '25502729') {
-			navigate('/naroceno');
+		// if (zaposleniID === '25502729') {
+		// 	navigate('/naroceno');
+		// }
+		const inputCode = inputRef.current.value;
+
+		try {
+			const fetchData = async () => {
+				const req = await fetch(
+					`https://portal.mikro-polo.si/api/users?X-API-KEY=${API_KEY}&access_code=${inputCode}`,
+					{
+						headers: {
+							'Content-Type': 'application/json',
+						},
+					}
+				);
+
+				if (!req.ok) {
+					if (req.status === 404) {
+						const error = 'Uporabnik s to kodo ne obstaja!';
+						// setErrorMsg(error);
+						throw new Error(error);
+					}
+					if (req.status === 403) {
+						const error = 'Dostop zavrnjen!';
+						// setErrorMsg(error);
+						throw new Error(error);
+					}
+				}
+
+				const userData = await req.json();
+				// setErrorMsg('');
+				// dispatch(authAction.logIn(userData));
+				navigate('/naroceno');
+			};
+
+			fetchData();
+		} catch (error) {
+			console.log(error);
 		}
 	};
 
@@ -34,6 +71,7 @@ const Prijava = () => {
 			<form onSubmit={submitFunctionHandler}>
 				<input
 					name='ID_zaposlenega'
+					type='password'
 					placeholder='ID zaposlenega'
 					onChange={idZaposlenegaInputHandler}
 					className={style.prijavaInput}
---
+++ src/private/API_KEYS.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'bf3e6919-8f7c-4a03-965f-ffe936476a76';
